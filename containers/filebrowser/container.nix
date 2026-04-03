7e8c8f96e1548f15a83237544afe5abc8ee17a94
Fady969 <Fatdrit.Shala@pm.me>
2026-04-03 20:05:28 +0200

    nixos: rebuild homelab 2026-04-03 20:05:28

--- containers/filebrowser/container.nix
+++ containers/filebrowser/container.nix
@@ -1,9 +1,30 @@
 { config, pkgs, ... }:
 
 let
-  dataDir = "/etc/nixos/containers/filebrowser/data";
+  dataDir = "/var/lib/filebrowser";
+	user = "filebrowser";
+	uid = "999";
 in
 {
+  #Create user and group
+  users.users.${user} = {
+    uid = 999;
+    isSystemUser = true;
+    group = "filebrowser";
+    createHome = false;
+    home = "/var/lib/filebrowser";
+    description = "FileBrowser service user";
+  };
+  
+  users.groups.${user} = {
+    gid = 999;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d ${dataDir} 0750 ${user} ${user} -"
+  ];
+
+ 
 	virtualisation.oci-containers = {
 		backend = "podman";
 		containers.filebrowser = {
@@ -18,6 +39,7 @@ in
 
 			extraOptions = [	
 				"--name=filebrowser"
+				"--user=${toString uid}:${toString uid}"
 			];
 			environment = {
 				"FB_PORT" = "8080";
